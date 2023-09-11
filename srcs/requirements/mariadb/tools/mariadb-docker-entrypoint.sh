8c6f484571649e96f6d696bd25db086df9fd7c94
Prad Rattanakijsoonthorn <pradrattana@gmail.com>
2023-09-12 04:11:55 +0700

    fixed error

--- srcs/requirements/mariadb/tools/mariadb-docker-entrypoint.sh
+++ srcs/requirements/mariadb/tools/mariadb-docker-entrypoint.sh
@@ -4,20 +4,35 @@ if [ ! -d "/var/lib/mysql/$MYSQL_DATABASE" ]
 then
 
 service mariadb start
+#systemctl start mariadb
+#systemctl enable mariadb
 
-echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" > init.sql
-echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> init.sql
-echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';" >> init.sql
+printf '\nn\nY\n$MYSQL_ROOT_PASSWORD\n$MYSQL_ROOT_PASSWORD\nY\nn\nY\nY' | mariadb-secure-installation
+
+echo "RENAME USER 'root'@'localhost' TO 'root'@'%';" >> init.sql
+echo "FLUSH PRIVILEGES;" >> init.sql
 echo "ALTER USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" >> init.sql
 echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;" >> init.sql
 echo "FLUSH PRIVILEGES;" >> init.sql
 
-mariadb -uroot < init.sql
-
+mariadb < init.sql
 rm init.sql
 
-service mariadb stop
+#echo "show grants for root@%; select user from mysql.user;" | mariadb -uroot -p$MYSQL_ROOT_PASSWORD
 
-fi
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" > init.sql
+echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> init.sql
+echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';" >> init.sql
+echo "FLUSH PRIVILEGES;" >> init.sql
+
+mariadb -uroot -p$MYSQL_ROOT_PASSWORD < init.sql
+rm init.sql
+
+#service mariadb stop
+#systemctl stop mariadb
+
+else
 
 mysqld
+
+fi
